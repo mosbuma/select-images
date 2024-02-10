6d2420f5dbd20105bf0ec1f6e3a876477c42bd88
Marc Buma <mosbuma@bumos.nl>
2024-02-10 14:31:43 +0100

    work in progress

--- users.ts
+++
@@ -1,5 +0,0 @@
-export const users = [
-    { id: '1', name: 'Marc', email: 'marcbuma@devilsshare.org', password: '3665' },
-    { id: '2', name: 'Jasper', email: 'jaspervis@devilsshare.org', password: '1887' },
-    { id: '3', name: 'Steven', email: 'stevenrietveld@devilsshare.org', password: '2398' }
-];
